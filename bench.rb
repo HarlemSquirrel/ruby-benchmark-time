25f72cbc5eebc42d305811e5d9e5f24e5cb62e6c
Kevin McCormack <harlemsquirrel@gmail.com>
2020-04-18 11:41:18 -0400

    Update benchmarks

--- bench.rb
+++ bench.rb
@@ -3,75 +3,54 @@
 require 'benchmark'
 require 'securerandom'
 
-n = 1_000_000
-
-puts "\n++ Loops"
+n = 10_000
 
 Benchmark.bmbm do |x|
-  x.report('for:')   { for i in 1..n; a = "1"; end }
-  x.report('times:') { n.times do   ; a = "1"; end }
-  x.report('upto:')  { 1.upto(n) do ; a = "1"; end }
-end
+  x.report("#{n} for loops")   { for i in 1..n; a = "1"; end }
+  x.report("#{n} times loops") { n.times { a = "1" } }
+  x.report("#{n} upto loops")  { 1.upto(n) {  a = "1" } }
 
-puts "\n++ String creation"
-
-Benchmark.bmbm do |x|
-  x.report('short') { n.times { 'short one' } }
-  x.report('long') do
+  x.report("#{n} short string creations") { n.times { 'short one' } }
+  x.report("#{n} long string creations") do
     n.times { 'abcdefghijklmnopqrstuvwxyz123456789)*&^%$#@!QWERTYUIOPLKJHGFDSAZXCVBNM,.\';][=-' }
   end
-end
+  x.report("#{n} 2^8 pack Unicode string creations") do
+    n.times { (0..2**8).to_a.pack('U*') }
+  end
 
-puts "\n++ Math"
+  x.report("#{n} Math.log")  { n.times { |i| Math.log i } }
+  x.report("#{n} Math.sqrt") { n.times { |i| Math.sqrt i } }
 
-Benchmark.bmbm do |x|
-  x.report('log')  { n.times { |i| Math.log i } }
-  x.report('sqrt') { n.times { |i| Math.sqrt i } }
-end
- 
-puts "\n++ Filesystem"
+  x.report("#{n} file listings")  { n.times { Dir.new('/var/log/') } }
+  x.report("#{n} File.write short") { n.times { |i| File.write('/tmp/ruby-benchmark-time', i) } }
+  x.report("#{n} File.read short") { n.times { |i| File.read('/tmp/ruby-benchmark-time', i) } }
 
-Benchmark.bmbm do |x|
-  x.report('list files') { n.times { Dir.new('/var/log/') } }
-end
+  x.report("#{n} Random.rand")     { n.times { Random.rand } }
+  x.report("#{n} Random.srand")    { n.times { Random.srand } }
+  [32, 64, 128].each do |size|
+    x.report("#{n} Random.urandom #{size}") { n.times { Random.urandom size } }
+    x.report("#{n} Random.urandom #{size} sort") { n.times { Random.urandom(size).split('').sort } }
+  end
 
-puts "\n++ Random"
+  [32, 64, 128, 256].each do |size|
+    x.report("#{n} SecureRandom.base64 #{size}") { n.times { SecureRandom.base64(size) } }
+    x.report("#{n} SecureRandom.base64 #{size} sort") { n.times { SecureRandom.base64(size).split('').sort } }
+    x.report("#{n} SecureRandom.hex #{size}")  { n.times { SecureRandom.hex(size) } }
+    x.report("#{n} SecureRandom.hex #{size} sort")  { n.times { SecureRandom.hex(size).split('').sort } }
+    x.report("#{n} SecureRandom.random bytes #{size}") { n.times { SecureRandom.random_bytes(size) } }
+    x.report("#{n} SecureRandom.random bytes #{size} sort") { n.times { SecureRandom.random_bytes(size).split('').sort } }
+  end
 
-Benchmark.bmbm do |x|
-  x.report('rand')     { n.times { Random.rand } }
-  x.report('srand')    { n.times { Random.srand } }
-  x.report('bytes 32') { n.times { Random.bytes 32 } }
-end
+  x.report("#{n} SecureRandom.uuid") { n.times { SecureRandom.uuid } }
 
-puts "\n++ SecureRandom"
-
-Benchmark.bmbm do |x|
-  x.report('hex 16')  { n.times { SecureRandom.hex(16) } }
-  x.report('hex 128') { n.times { SecureRandom.hex(128) } }
-
-  x.report('random bytes 64')  { n.times { SecureRandom.random_bytes(64) } }
-  x.report('random bytes 256') { n.times { SecureRandom.random_bytes(256) } }
-
-  x.report('uuid') { n.times { SecureRandom.uuid } }
-end
-
-puts "\n++ SecureRandom and sorting"
-
-Benchmark.bmbm do |x|
-  x.report('base64 128 sort') { n.times { SecureRandom.base64(128).split('').sort } }
-  x.report('base64 256 sort') { n.times { SecureRandom.base64(256).split('').sort } }
-end
-
-puts "\n++ SecureRandom and searching"
-
-Benchmark.bmbm do |x|
-  x.report('base64 1 bsearch') do
+  x.report("#{n} SecureRandom.base64 1 bsearch") do
     n.times do
       char = SecureRandom.base64(1)
       (('a'..'z').to_a + ('A'..'Z').to_a + (1..9).to_a).bsearch { |e| e == char }
     end
   end
-  x.report('base64 1 find_index') do
+
+  x.report("#{n} SecureRandom.base64 1 find_index") do
     n.times do
       (('a'..'z').to_a + ('A'..'Z').to_a + (1..9).to_a).find_index SecureRandom.base64(1)
     end
