7887170e0559da95955adc915439abd368f9568b
Kevin McCormack <harlemsquirrel@gmail.com>
2020-04-18 11:53:35 -0400

    Update benchmarks

--- bench.rb
+++ bench.rb
@@ -21,18 +21,25 @@ Benchmark.bmbm do |x|
   x.report("#{n} Math.log")  { n.times { |i| Math.log i } }
   x.report("#{n} Math.sqrt") { n.times { |i| Math.sqrt i } }
 
-  x.report("#{n} file listings")  { n.times { Dir.new('/var/log/') } }
+  x.report("#{n} directory listings")  { n.times { Dir.new('/var/log/') } }
   x.report("#{n} File.write short") { n.times { |i| File.write('/tmp/ruby-benchmark-time', i) } }
   x.report("#{n} File.read short") { n.times { |i| File.read('/tmp/ruby-benchmark-time', i) } }
+  x.report("#{n} File.write 2**16 chars") do
+    string = (0..2**16).to_a.pack('U*')
+    n.times { |i| File.write('/tmp/ruby-benchmark-time', string) }
+  end
+  x.report("#{n} File.read 2**16 chars") do
+    n.times { |i| File.read('/tmp/ruby-benchmark-time') }
+  end
 
   x.report("#{n} Random.rand")     { n.times { Random.rand } }
   x.report("#{n} Random.srand")    { n.times { Random.srand } }
-  [32, 64, 128].each do |size|
+  [32, 64, 128, 256, 512].each do |size|
     x.report("#{n} Random.urandom #{size}") { n.times { Random.urandom size } }
     x.report("#{n} Random.urandom #{size} sort") { n.times { Random.urandom(size).split('').sort } }
   end
 
-  [32, 64, 128, 256].each do |size|
+  [32, 64, 128, 256, 512].each do |size|
     x.report("#{n} SecureRandom.base64 #{size}") { n.times { SecureRandom.base64(size) } }
     x.report("#{n} SecureRandom.base64 #{size} sort") { n.times { SecureRandom.base64(size).split('').sort } }
     x.report("#{n} SecureRandom.hex #{size}")  { n.times { SecureRandom.hex(size) } }
