ac8e8305a43cdbdb9ef3855ef35a0f240a61eae7
Kevin McCormack <harlemsquirrel@gmail.com>
2020-04-18 10:24:53 -0400

    Update benchmarks

--- bench.rb
+++ bench.rb
@@ -3,32 +3,47 @@
 require 'benchmark'
 require 'securerandom'
 
-n = 5_000_000
+n = 1_000_000
 
-puts "\nSet variable in loop"
+puts "\n++ Loops"
 
 Benchmark.bmbm do |x|
-  x.report("for:")   { for i in 1..n; a = "1"; end }
-  x.report("times:") { n.times do   ; a = "1"; end }
-  x.report("upto:")  { 1.upto(n) do ; a = "1"; end }
+  x.report('for:')   { for i in 1..n; a = "1"; end }
+  x.report('times:') { n.times do   ; a = "1"; end }
+  x.report('upto:')  { 1.upto(n) do ; a = "1"; end }
 end
 
-puts "\nString creation"
+puts "\n++ String creation"
 
 Benchmark.bmbm do |x|
   x.report('short') { n.times { 'short one' } }
-  x.report('long')  { n.times { 'abcdefghijklmnopqrstuvwxyz123456789)*&^%$#@!QWERTYUIOPLKJHGFDSAZXCVBNM,.\';][=-' } }
+  x.report('long') do
+    n.times { 'abcdefghijklmnopqrstuvwxyz123456789)*&^%$#@!QWERTYUIOPLKJHGFDSAZXCVBNM,.\';][=-' }
+  end
 end
 
+puts "\n++ Math"
 
-puts "\nFilesystem"
+Benchmark.bmbm do |x|
+  x.report('log')  { n.times { |i| Math.log i } }
+  x.report('sqrt') { n.times { |i| Math.sqrt i } }
+end
+ 
+puts "\n++ Filesystem"
 
 Benchmark.bmbm do |x|
   x.report('list files') { n.times { Dir.new('/var/log/') } }
 end
 
+puts "\n++ Random"
 
-puts "\nSecureRandom"
+Benchmark.bmbm do |x|
+  x.report('rand')     { n.times { Random.rand } }
+  x.report('srand')    { n.times { Random.srand } }
+  x.report('bytes 32') { n.times { Random.bytes 32 } }
+end
+
+puts "\n++ SecureRandom"
 
 Benchmark.bmbm do |x|
   x.report('hex 16')  { n.times { SecureRandom.hex(16) } }
@@ -38,4 +53,27 @@ Benchmark.bmbm do |x|
   x.report('random bytes 256') { n.times { SecureRandom.random_bytes(256) } }
 
   x.report('uuid') { n.times { SecureRandom.uuid } }
+end
+
+puts "\n++ SecureRandom and sorting"
+
+Benchmark.bmbm do |x|
+  x.report('base64 128 sort') { n.times { SecureRandom.base64(128).split('').sort } }
+  x.report('base64 256 sort') { n.times { SecureRandom.base64(256).split('').sort } }
+end
+
+puts "\n++ SecureRandom and searching"
+
+Benchmark.bmbm do |x|
+  x.report('base64 1 bsearch') do
+    n.times do
+      char = SecureRandom.base64(1)
+      (('a'..'z').to_a + ('A'..'Z').to_a + (1..9).to_a).bsearch { |e| e == char }
+    end
+  end
+  x.report('base64 1 find_index') do
+    n.times do
+      (('a'..'z').to_a + ('A'..'Z').to_a + (1..9).to_a).find_index SecureRandom.base64(1)
+    end
+  end
 end
